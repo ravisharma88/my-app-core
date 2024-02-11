90e5e8b0072f189d24de30706769780fe0fc35cc
ravisharma <ravi.neo02@gmail.com>
2024-02-10 20:29:28 -0500

    Initial commit.

---
+++ src/main/java/com/ravi/myapp/core/MyappCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.myapp.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/myapp/core/MyappCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.myapp.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
